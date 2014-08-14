a10b6f098c360d96af336977c6b83ff7067327d9
Manuel Vulp <manuelvulp@gmail.com>
2014-08-14 12:18:28 +0300

    tests, coverage 100%

--- src/object-transformer.js
+++ src/object-transformer.js
@@ -1,62 +1,64 @@
 var Add = function (key, value) {
-  this.schema[key] = key;
-  return this;
+    this.schema[key] = value;
+    return this;
 };
 
 var Remove = function (key) {
-  delete this.schema[key];
-  return this;
+    delete this.schema[key];
+    return this;
 };
 
 var Single = function (object, schema) {
-  if (!object instanceof Object) throw new Error('Transformer received no valid object');
-  if (object instanceof Array) throw new Error('Transformer received no valid object');
-  this.object = object;
-  if (!schema) throw new Error('Transformer received no valid schema');
-  this.schema = schema;
-  return this;
+    if (!object instanceof Object) throw new Error('Transformer received no valid object');
+    if (object instanceof Array) throw new Error('Transformer received no valid object');
+    this.object = object;
+    if (!schema) throw new Error('Transformer received no valid schema');
+    this.schema = schema;
+    return this;
 };
 
 Single.prototype.add = Add;
 Single.prototype.remove = Remove;
 Single.prototype.parse = function () {
-  var response = {};
-  for (var index in this.schema) {
-    var split = this.schema[index].split('.');
-    var value = this.object[split[0]];
-    var breadcrumbs = split[0];
+    var response = {};
+    for (var index in this.schema) {
+        var split = this.schema[index].split('.');
+        var value = this.object[split[0]];
+        var breadcrumbs = split[0];
 
-    if (!value && split.length > 1) throw new Error('Malformed object, missing attribute "' +
-          breadcrumbs + '" when trying to get attribute ' +
-          breadcrumbs + '[' + split[1] + ']');
+        if (!value && split.length > 1) {
+            throw new Error('Malformed object, missing attribute "' +
+                breadcrumbs + '" when trying to get attribute ' +
+                breadcrumbs + '[' + split[1] + ']');
+        }
 
-    for (var i = 1; i < split.length; i++) {
-      breadcrumbs += '[' + split[i] + ']';
-      if (!value[split[i]] && i + 1 !== split.length) {
-        throw new Error('Malformed object, missing attribute "' +
-          breadcrumbs + '" when trying to get attribute ' +
-          breadcrumbs + '[' + split[i + 1] + ']');
-      }
-      value = value[split[i]];
+        for (var i = 1; i < split.length; i++) {
+            breadcrumbs += '[' + split[i] + ']';
+            if (!value[split[i]] && i + 1 !== split.length) {
+                throw new Error('Malformed object, missing attribute "' +
+                    breadcrumbs + '" when trying to get attribute ' +
+                    breadcrumbs + '[' + split[i + 1] + ']');
+            }
+            value = value[split[i]];
+        }
+        response[index] = value;
     }
-    response[index] = value;
-  }
-  return response;
+    return response;
 };
 
 var List = function (array, schema) {
-  if (!array instanceof Array) throw new Error('Transformer received no valid array');
-  this.array = array;
-  if (!schema) throw new Error('Transformer received no valid schema');
-  this.schema = schema;
+    if (!array instanceof Array) throw new Error('Transformer received no valid array');
+    this.array = array;
+    if (!schema) throw new Error('Transformer received no valid schema');
+    this.schema = schema;
 };
 
 List.prototype.parse = function () {
-  var response = [];
-  for (var i in this.array) {
-    response.push(new Single(this.array[i], this.schema).parse())
-  }
-  return response;
+    var response = [];
+    for (var i in this.array) {
+        response.push(new Single(this.array[i], this.schema).parse())
+    }
+    return response;
 };
 
 List.prototype.add = Add;
--- test/src/object-transformerSpec.js
+++ test/src/object-transformerSpec.js
@@ -2,51 +2,145 @@ var path = require('path');
 var expect = require('must');
 var Transformer = require(path.join(process.cwd(), 'src', 'object-transformer'));
 
-var models = [
-    {
-        id: '00eb000a0de000b000baa0ea',
-        user: {
-            id: '11eb111a1de111b111baa1eb',
-            name: 'John',
-            address: {
-                street: 'FooStreet',
-                house: 1,
-                appartment: 2
-            }
-        },
-        message: 'bar'
-    },
-    {
-        id: '00eb000a0de000b000baa0ea',
-        user: {
-            id: '11eb111a1de111b111baa1eb',
-            name: 'Doe',
-            address: {
-                street: 'BarStreet',
-                house: 3,
-                appartment: 4
-            }
-        },
-        message: 'baz'
-    }
-];
-
-var singleSchema = {
-    'title': 'user.name',
-    'street': 'user.address.street'
-};
-
-var listSchema = {
-    'message': 'message',
-    'address': 'user.address'
-};
-
 describe('Transformer', function () {
+
+    var model;
+    var models;
+    var schema;
+
+    beforeEach(function () {
+        model = {
+            id: '00eb000a0de000b000baa0ea',
+            user: {
+                id: '11eb111a1de111b111baa1eb',
+                name: 'John',
+                address: {
+                    street: 'FooStreet',
+                    house: 1,
+                    appartment: 2
+                }
+            },
+            message: 'bar'
+        };
+
+        models = [
+            model,
+            {
+                id: '00eb000a0de000b000baa0ea',
+                user: {
+                    id: '11eb111a1de111b111baa1eb',
+                    name: 'Doe',
+                    address: {
+                        street: 'BarStreet',
+                        house: 3,
+                        appartment: 4
+                    }
+                },
+                message: 'baz'
+            }
+        ];
+
+        schema = {
+            'title': 'user.name',
+            'street': 'user.address.street'
+        };
+    });
 
     describe('Single', function () {
 
-        it('should throw error if no schema is specified', function () {
-            expect(new Transformer.Single()).to.throw();
+        it('should throw if no object is given', function () {
+            expect(function () {
+                new Transformer.Single();
+            }).to.throw();
+        });
+
+        it('should throw if no schema is given', function () {
+            expect(function () {
+                new Transformer.Single(model);
+            }).to.throw();
+        });
+
+        it('should throw if array is given instead of an object', function () {
+            expect(function () {
+                new Transformer.Single(models);
+            }).to.throw();
+        });
+
+        it('should accept valid object and schema', function () {
+            expect(function () {
+                new Transformer.Single(model, schema);
+            }).to.not.throw();
+        });
+
+        it('should add new key to schema', function () {
+            var transformer = new Transformer.Single(model, schema);
+            transformer.add('test', 'user.id');
+            transformer.schema['test'].must.equal('user.id');
+        });
+
+        it('should remove key from schema', function () {
+            var transformer = new Transformer.Single(model, schema);
+            transformer.remove('title');
+            expect(transformer.schema['title']).to.be.undefined();
+        });
+
+        it('should parse valid response from object based on schema', function () {
+            var transformer = new Transformer.Single(model, schema);
+            var parsed = transformer.parse();
+            parsed.must.have.keys(['title', 'street']);
+        });
+
+        it('should throw if trying to parse object with missing key', function () {
+            model = {
+                this: 'fails'
+            };
+            schema = {
+                'title': 'certainly.fails'
+            };
+            var transformer = new Transformer.Single(model, schema);
+            expect(function () {
+                transformer.parse();
+            }).to.throw();
+        });
+
+        it('should throw if trying to parse object with missing deep key', function () {
+            model = {
+                this: 'fails'
+            };
+            schema = {
+                'title': 'this.certainly.fails'
+            };
+            var transformer = new Transformer.Single(model, schema);
+            expect(function () {
+                transformer.parse();
+            }).to.throw();
+        });
+    });
+
+    describe('List', function () {
+
+        it('should throw if no array is given', function () {
+            expect(function () {
+                new Transformer.List();
+            }).to.throw();
+        });
+
+        it('should throw if no schema is given', function () {
+            expect(function () {
+                new Transformer.List(models);
+            }).to.throw();
+        });
+
+        it('should accept valid array and schema', function () {
+            expect(function () {
+                new Transformer.List(models, schema);
+            }).to.not.throw();
+        });
+
+        it('should parse all array members', function () {
+            var transformer = new Transformer.List(models, schema);
+            var parsed = transformer.parse();
+            expect(parsed.length).to.equal(2);
         });
     });
 
